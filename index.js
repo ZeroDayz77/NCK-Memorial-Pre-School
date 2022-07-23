5f44077119e5511cc3c67018ec5fc76461056563
zerodayz77 <charisyt26@gmail.com>
2022-07-22 22:20:08 -0400

    added a remote private repo for deployment testing and added more functionality to the web pages and navbar

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // for navbar
 $(document).ready(function () {
-    $('.navbar-light .dmenu').hover(function () {
-            $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
-        }, function () {
-            $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
-        });
-    });
+  $(".navbar-light .dmenu").hover(
+    function () {
+      $(this).find(".sm-menu").first().stop(true, true).slideDown(150);
+    },
+    function () {
+      $(this).find(".sm-menu").first().stop(true, true).slideUp(105);
+    }
+  );
+});
+
+$(document).ready(function () {
+  $(".second-button").on("click", function () {
+    $(".animated-icon2").toggleClass("open");
+  });
+});
